3515a21ca2642ff6607870df0b9aecadab557e01
Dante Herrera <dherrera3@my.wctc.edu>
2024-03-27 20:26:51 -0500

    separate tickets into lists

--- TicketAdd.cs
+++ TicketAdd.cs
@@ -1,34 +1,37 @@
 public class TicketAdd
 {
     public string filePath { get; set; }
-    public List<Ticket> tickets { get; set; }
+    public List<BugTicket> bugTickets { get; set; }
+    public List<EnhancementTicket> enhancementTickets { get; set; }
+    public List<TaskTicket> taskTickets { get; set; }
+
     public TicketAdd(string ticketFilePath)
     {
         filePath = ticketFilePath;
 
-        tickets = new List<Ticket>();
+        bugTickets = new List<BugTicket>();
 
         StreamReader sr = new StreamReader(filePath);
         while (!sr.EndOfStream)
         {
-            string line = sr.ReadLine();
+            // string line = sr.ReadLine();
 
-            string[] tickArr = line.Split(',');
+            // string[] tickArr = line.Split(',');
 
-            // Console.WriteLine(tickArr[4]);
+            // // Console.WriteLine(tickArr[4]);
 
-            Ticket ticket = new Ticket
-            {
-                tickID = tickArr[0],
-                summary = tickArr[1],
-                status = tickArr[2],
-                priority = tickArr[3],
-                submitter = tickArr[4],
-                assigner = tickArr[5],
-                watched = tickArr[6]
-            };
+            // Ticket ticket = new Ticket
+            // {
+            //     tickID = tickArr[0],
+            //     summary = tickArr[1],
+            //     status = tickArr[2],
+            //     priority = tickArr[3],
+            //     submitter = tickArr[4],
+            //     assigner = tickArr[5],
+            //     watched = tickArr[6]
+            // };
 
-            tickets.Add(ticket);
+            // tickets.Add(ticket);
         }
         sr.Close();
 
@@ -41,7 +44,7 @@ public class TicketAdd
         sw.Write($"{ticket.tickID},{ticket.summary},{ticket.status},{ticket.priority},{ticket.submitter},{ticket.assigner},{ticket.watched},{ticket.severity}");
         sw.Close();
 
-        tickets.Add(ticket);
+        bugTickets.Add(ticket);
     }
 
     public void addEnhancement(EnhancementTicket ticket)
@@ -51,7 +54,7 @@ public class TicketAdd
         sw.Write($"{ticket.tickID},{ticket.summary},{ticket.status},{ticket.priority},{ticket.submitter},{ticket.assigner},{ticket.watched},{ticket.software},{ticket.cost},{ticket.reason},{ticket.estimate}");
         sw.Close();
 
-        tickets.Add(ticket);
+        enhancementTickets.Add(ticket);
     }
 
     public void addTask(TaskTicket ticket)
@@ -61,6 +64,6 @@ public class TicketAdd
         sw.Write($"{ticket.tickID},{ticket.summary},{ticket.status},{ticket.priority},{ticket.submitter},{ticket.assigner},{ticket.watched},{ticket.projectName},{ticket.dueDate}");
         sw.Close();
 
-        tickets.Add(ticket);
+        taskTickets.Add(ticket);
     }
 }
